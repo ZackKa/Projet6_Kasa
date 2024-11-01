c9d330327ff086fd4fb7a18018a8922597932896
Zakari Ka <zakari_kaddour@live.fr>
2024-11-01 10:43:06 +0100

    changement style

--- src/pages/About/index.jsx
+++ src/pages/About/index.jsx
@@ -21,7 +21,7 @@ function About() {
                         <p className='contenue__respect'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
                     </Collapse>
                     <Collapse label="Service">
-                        <p className='contenue__service'>La bienveillance fait partie des valeurs fondatrices de Kasa. Tout comportement discriminatoire ou de perturbation du voisinage entraînera une exclusion de notre plateforme.</p>
+                        <p className='contenue__service'>La qualité de service est au coeur de notre engagement chez Kasa. Nous veillons à ce que chaque interaction que ce soit avec nos hôtes ou nos locataires, soit empreinte de respect et de bienveillance.</p>
                     </Collapse>
                     <Collapse label="Securite">
                         <p className='contenue__securite'>La sécurité est la priorité de Kasa. Aussi bien pour nos hôtes que les voyageurs, chaque logement correspond aux critères de sécurité établis par nos services. En laissant une note aussi bien à l'hôte qu'au locataire, cela permet à nos équipes de vérifier que les standards sont bien respectés. Nous organisons également des ateliers sur la sécurité domestique pour nos hôtes.</p>
